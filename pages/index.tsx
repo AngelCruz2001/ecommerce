bb750dd5992d99e9fb6813676ef1bd3f7ceb36ec
Ángel Cruz García <angi_ed01@hotmail.com>
2022-07-18 01:14:02 -0500

    Canary added

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,8 +27,8 @@ export default function Home({ products }: IProductListProps) {
                 {/* <Contact/> */}
             </main>
             <Script src="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.js" />
-            <div hidden id="snipcart" data-api-key="OWNlZTNlYmItZTU5OS00MmI4LThjOTAtMjY0NzAyYmI2ODAxNjM3NjIyMjY3MDMxMTIzMzUx
-"></div>
+            <div hidden id="snipcart" data-api-key="OWNlZTNlYmItZTU5OS00MmI4LThjOTAtMjY0NzAyYmI2ODAxNjM3NjIyMjY3MDMxMTIzMzUx">
+            </div>
         </>
     )
 }
